07152af1fc295a887dcbbd841c78b5d1784b349c
x3imal <aasurov@gmail.com>
2023-02-28 15:09:40 +0300

    task4 completed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
 
     }
 
     public static void task1() {
+        System.out.println("Задача 1");
 
         int[] nums = new int[]{1, 2, 3};
         float[] nums1 = {1.57f, 7.654f, 9.986f};
@@ -15,6 +19,7 @@ public class Main {
     }
 
     public static void task2(){
+        System.out.println("Задача 2");
 
         int[] nums = new int[] {1, 2, 3};
         System.out.println(nums[0] + ", " + nums[1] + ", " + nums[2]);
@@ -27,6 +32,7 @@ public class Main {
     }
 
     public static void task3(){
+        System.out.println("Задача 3");
 
         int[] nums3 = new int[] {1, 2, 3};
         System.out.println(nums3[2] + ", " + nums3[1] + ", " + nums3[0]);
@@ -39,5 +45,15 @@ public class Main {
 
     }
 
+    public static void task4(){
+        System.out.println("Задача 4");
 
+        int[] nums6 = new int[] {1, 2, 3};
+        for (int i = 0; i < nums6.length; i++) {
+            if(nums6[i] % 2 != 0) {
+                nums6[i] = nums6[i] + 1;
+            }
+        }
+        System.out.println(Arrays.toString(nums6));
+    }
 }
