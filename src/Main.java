c5c263a03c2dd520dbcb053dac07cea3fd11b657
x3imal <aasurov@gmail.com>
2023-02-28 14:24:21 +0300

    task2 completed

--- src/Main.java
+++ src/Main.java
@@ -13,4 +13,20 @@ public class Main {
         byte[] nums2 = new byte[3]; //по умолчанию все 3 элемента это - 0
 
     }
+
+    public static void task2(){
+
+        int[] nums = new int[] {1, 2, 3};
+        System.out.println(nums[0] + "," + nums[1] + "," + nums[2]);
+
+        float[] nums1 =  {1.57f, 7.654f, 9.986f};
+        System.out.println(nums1[0] + "," + nums1[1] + "," + nums1[2]);
+
+        byte[] nums2 = new byte[3];
+        System.out.println(nums2[0] + "," + nums2[1] + "," + nums2[2]);
+    }
+
+    public static void task3(){
+
+    }
 }
