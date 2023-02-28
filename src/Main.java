c219481652f0e174ef3cd0980f9f7d561e56e989
x3imal <aasurov@gmail.com>
2023-02-28 14:23:45 +0300

    task1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+
+    }
+
+    public static void task1() {
+
+        int[] nums = new int[]{1, 2, 3};
+        float[] nums1 = {1.57f, 7.654f, 9.986f};
+        byte[] nums2 = new byte[3]; //по умолчанию все 3 элемента это - 0
+
     }
 }
