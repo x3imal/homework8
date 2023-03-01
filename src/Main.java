b48954975b727e8b15415829742ce71a20656e5b
x3imal <aasurov@gmail.com>
2023-03-01 13:07:30 +0300

    task2 completed (editing)

--- src/Main.java
+++ src/Main.java
@@ -4,56 +4,48 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        task3();
-        task4();
+        //task3();
+        //task4();
 
     }
 
     public static void task1() {
         System.out.println("Задача 1");
 
-        int[] nums = new int[]{1, 2, 3};
+        int[] nums = new int[3];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+
         float[] nums1 = {1.57f, 7.654f, 9.986f};
         byte[] nums2 = new byte[3]; //по умолчанию все 3 элемента это - 0
 
     }
 
-    public static void task2(){
+    public static void task2() {
         System.out.println("Задача 2");
 
-        int[] nums = new int[] {1, 2, 3};
-        System.out.println(nums[0] + ", " + nums[1] + ", " + nums[2]);
-
-        float[] nums1 =  {1.57f, 7.654f, 9.986f};
-        System.out.println(nums1[0] + ", " + nums1[1] + ", " + nums1[2]);
-
-        byte[] nums2 = new byte[3];
-        System.out.println(nums2[0] + ", " + nums2[1] + ", " + nums2[2]);
-    }
-
-    public static void task3(){
-        System.out.println("Задача 3");
-
-        int[] nums3 = new int[] {1, 2, 3};
-        System.out.println(nums3[2] + ", " + nums3[1] + ", " + nums3[0]);
-
-        float[] nums4 =  {1.57f, 7.654f, 9.986f};
-        System.out.println(nums4[2] + ", " + nums4[1] + ", " + nums4[0]);
-
-        byte[] nums5 = new byte[3];
-        System.out.println(nums5[2] + ", " + nums5[1] + ", " + nums5[0]);
-
-    }
-
-    public static void task4(){
-        System.out.println("Задача 4");
-
-        int[] nums6 = new int[] {1, 2, 3};
-        for (int i = 0; i < nums6.length; i++) {
-            if(nums6[i] % 2 != 0) {
-                nums6[i] = nums6[i] + 1;
+        int[] nums = new int[3];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == nums.length - 1) {
+                System.out.println(nums[i] + " ");
+                break;
             }
+            System.out.print(nums[i] + ", ");
         }
-        System.out.println(Arrays.toString(nums6));
+
+        float[] nums1 = {1.57f, 7.654f, 9.986f};
+        for(int i = 0; i < nums1.length; i ++) {
+            if(i == nums1.length - 1) {
+                System.out.println(nums1[i] + " ");
+                break;
+            }
+            System.out.print(nums1[i] + ", ");
+        }
     }
 }
+
+
